762e9b556be6843142e10794073b813aa1c209a9
Roger Howard <rogerhoward@gmail.com>
2016-02-16 16:22:34 -0800

    Refactored to use print() and format()

--- issuefs.py
+++ issuefs.py
@@ -3,16 +3,13 @@
 # Based on MIT-licensed code from:
 # https://www.stavros.io/posts/python-fuse-filesystem/
 
-import os
-import sys
-import errno
+import os, sys, errno
 import click
-import keyring
-
 from github3 import login
 from fuse import FUSE, FuseOSError, Operations
 
-debug = False
+debug = True
+
 
 class IssueFS(Operations):
     def __init__(self, repo, username, password):
@@ -23,7 +20,7 @@ class IssueFS(Operations):
     def _contents(self, path):
         # Return contents of a given path
         # For now, return the issue title and body concatenated
-        if debug: print '_contents path: %s' % (path)
+        if debug: print('_contents path: {}'.format(path))
 
         # Extract issue from filepath, coerce to int; if fails, abort
         try:
@@ -31,38 +28,40 @@ class IssueFS(Operations):
         except:
             return False
 
-        if debug: print '_contents issue: %s' % (issue)
+        if debug: print('_contents issue: {}'.format(issue))
 
         # Get Github issue
         gh = login(self.username, self.password)
         issue = gh.issue(self.username, self.repo, issue)
 
         # Concatanate issue title and body and return as file content
-        issue_contents = '%s \n %s' % (issue.title, issue.body)
-        if debug: print '_contents issue_contents: %s' % (issue_contents)
+        issue_contents = '{} \n {}'.format(issue.title, issue.body)
+        if debug: print('_contents issue_contents: {}'.format(issue_contents))
         return issue_contents
 
     # Filesystem methods
     # ==================
 
     def access(self, path, mode):
-        if debug: print 'access mode: %s, path: %s ' % (mode, path)
+        if debug: print('access mode: {}, path: {}'.format(mode, path))
         # Mock
         if False:
             raise FuseOSError(errno.EACCES)
 
     def chmod(self, path, mode):
-        if debug: print 'chmod mode: %s, path: %s ' % (mode, path)
+        if debug: print('chmod mode: {}, path: {}'.format(mode, path))
         # Mock
         return True
 
     def chown(self, path, uid, gid):
-        if debug: print 'chown uid: %s, gid: %s, path: %s ' % (uid, gid, path)
+        if debug: print('chown uid: {}, gid: {}, path: {}'.format(uid, gid, path))
         # Mock
         return True
 
     def getattr(self, path, fh=None):
-        if debug: print 'getattr path: %s' % (path)
+        if debug: print('getattr path: {}'.format(path))
+
+        ignore_paths = ['/._.', '.gitignore']
 
         # If path is a directory
         if path.endswith('/'):
@@ -77,7 +76,7 @@ class IssueFS(Operations):
         return s
 
     def readdir(self, path, fh):
-        if debug: print 'readdir path: %s' % (path)
+        if debug: print('readdir path: {}'.format(path))
 
         # If root of device...
         if path == '/':
@@ -89,7 +88,7 @@ class IssueFS(Operations):
 
             # Add issue "filename" to children array
             for issue in gh.iter_repo_issues(self.username, self.repo):
-                issue_filename = ('%s_%s' % (issue.number, issue.title))[:255]
+                issue_filename = '{}_{}'.format(issue.number, issue.title)[:255]
                 children.append(issue_filename)
 
             # Return a generator object for each entry in children
@@ -98,53 +97,53 @@ class IssueFS(Operations):
                 yield entry
 
     def readlink(self, path):
-        if debug: print 'readlink path: %s' % (path)
+        if debug: print('readlink path: {}'.format(path))
         # Mock
         return path
 
     def mknod(self, path, mode, dev):
-        if debug: print 'mknod path: %s' % (path)
+        if debug: print('mknod path: {}'.format(path))
         # Mock
         return
 
     def rmdir(self, path):
-        if debug: print 'rmdir path: %s' % (path)
+        if debug: print('rmdir path: {}'.format(path))
         # Mock
         return path
 
     def mkdir(self, path, mode):
-        if debug: print 'mkdir path: %s' % (path)
+        if debug: print('mkdir path: {}'.format(path))
         # Mock
         return
 
     def statfs(self, path):
-        if debug: print 'statfs path: %s' % (path)
+        if debug: print('statfs path: {}'.format(path))
         # Mocked up statfs return values for now
         # Mostly nonsensical but functional
         return {'f_bsize': 1048576, 'f_bavail': 0, 'f_favail': 7745916, 'f_files': 3, 'f_frsize': 4096, 'f_blocks': 29321728, 'f_ffree': 7745916, 'f_bfree': 0, 'f_namemax': 255, 'f_flag': 0}
 
     def unlink(self, path):
-        if debug: print 'unlink path: %s' % (path)
+        if debug: print('unlink path: {}'.format(path))
         # Mock
         return
 
     def symlink(self, name, target):
-        if debug: print 'symlink path: %s' % (path)
+        if debug: print('symlink name: {}, target: {}'.format(name, target))
         # Mock
         return
 
     def rename(self, old, new):
-        if debug: print 'rename path: %s' % (path)
+        if debug: print('rename old: {}, new: {}'.format(old, new))
         # Mock
         return
 
     def link(self, target, name):
-        if debug: print 'link path: %s' % (path)
+        if debug: print('link name: {}, target: {}'.format(name, target))
         # Mock
         return
 
     def utimens(self, path, times=None):
-        if debug: print 'utimens path: %s' % (path)
+        if debug: print('utimens path: {}'.format(path))
         # Mock
         return
 
@@ -152,43 +151,43 @@ class IssueFS(Operations):
     # ============
 
     def open(self, path, flags):
-        if debug: print 'open path: %s' % (path)
+        if debug: print('open path: {}'.format(path))
         # Mock
         return True
 
     def create(self, path, mode, fi=None):
-        if debug: print 'create path: %s' % (path)
+        if debug: print('create path: {}, mode: {}'.format(path, mode))
         # Mock
         return
 
     def read(self, path, length, offset, fh):
-        if debug: print 'read data from %s - %s:%s' % (path, length, offset)
+        if debug: print('read path: {} - {}:{}'.format(path, length, offset))
         # Retrieve contents, apply read offsets, and return
         # Added str() explicitly to return a byte array which read() expects
         return str(self._contents(path)[offset:offset+length])
 
     def write(self, path, buf, offset, fh):
-        if debug: print 'write path: %s' % (path)
+        if debug: print('write path: {}, offset: {}'.format(path, offset))
         # Mock
         return True
 
     def truncate(self, path, length, fh=None):
-        if debug: print 'truncate path: %s' % (path)
+        if debug: print('truncate path: {}, length: {}'.format(path, length))
         # Mock
         return True
 
     def flush(self, path, fh):
-        if debug: print 'flush path: %s' % (path)
+        if debug: print('flush path: {}'.format(path))
         # Mock
         return True
 
     def release(self, path, fh):
-        if debug: print 'release path: %s' % (path)
+        if debug: print('release path: {}'.format(path))
         # Mock
         return True
 
     def fsync(self, path, fdatasync, fh):
-        if debug: print 'fsync path: %s' % (path)
+        if debug: print('fsync path: {}'.format(path))
         # Mock
         return True
 
